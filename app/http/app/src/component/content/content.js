1cbe2ed868928c09c5faa35c7f015349482515ed
wisemuji <wisemuji@gmail.com>
2019-04-14 23:40:16 +0900

    linked to google play store

--- app/http/app/src/component/content/content.js
+++ app/http/app/src/component/content/content.js
@@ -10,7 +10,9 @@ export const Content = ()=> {
                 <span className = "intro1">PREPARE FOR YOUR</span>
                 <br/>
                 <span className = "intro2">PRESENT TIME</span>
-                <img className="googleImg" src={Google}/>
+                <a href="https://play.google.com/store/apps/details?id=s2017s25.kr.hs.mirim.present_2018stac">
+                    <img className="googleImg" src={Google}/>
+                </a>
             </div>
             <div className = "product">
                 <img className="productImg" src={Product}/>
