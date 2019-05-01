6ac3bab965397396f85f68127619d3fc6355ac66
Suhyeon Kim <wisemuji@gmail.com>
2019-05-01 12:03:37 +0900

    reviews section structure

--- app/http/app/src/component/reviews/reviews.js
+++ app/http/app/src/component/reviews/reviews.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react'
+import Quote from '../../assets/quote.png'
 import './reviews.css'
 
 export const Reviews = ()=> {
     return (
         <section id = "reviews">
-            <div className = "contents">
-                <span className = "description">reviews</span>
+            <div className = "summary">
+                <h2>What People are Saying</h2>
+                <p>People are commenting good stories about us in Google Play Store. We love Reviews!!</p>
+            </div>
+            <div className = "comments">
+                <div className = "comment-wrap">
+                    <div className = "comment-box">
+                        <img className = "comment-img" src={Quote} alt="quote"/>
+                        <p className = "comment-text">
+                            "very good app very good app very good app very good app very good app"
+                        </p>
+                    </div>
+                    <div className = "tri"></div>
+                </div>
+                <div className = "comment-wrap">
+                    <div className = "comment-box">
+                        <img className = "comment-img" src={Quote} alt="quote"/>
+                        <p className = "comment-text">
+                            "very good app very good app very good app very good app very good app"
+                        </p>
+                    </div>
+                    <div className = "tri"></div>
+                </div>
+                <div className = "comment-wrap">
+                    <div className = "comment-box">
+                        <img className = "comment-img" src={Quote} alt="quote"/>
+                        <p className = "comment-text">
+                            "very good app very good app very good app very good app very good app"
+                        </p>
+                    </div>
+                    <div className = "tri"></div>
+                </div>
             </div>
         </section>
     )
