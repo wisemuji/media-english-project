5b9038af2d0b32c063cb38d4dfd67a491b5f2ed7
Suhyeon Kim <wisemuji@gmail.com>
2019-05-23 15:53:52 +0900

    Form Design ready

--- app/http/app/src/component/communicate/communicate.js
+++ app/http/app/src/component/communicate/communicate.js
@@ -5,8 +5,19 @@ export const Communicate = ()=> {
     return (
         <section id = "communicate">
             <div className = "summary">
-                <h2>Be in Touch</h2>
-                <p>We want to hear from you!</p>
+                <h2>Contact Us</h2>
+                <p>We want to hear from you! Feel free to conteact us.</p>
+            </div>
+            <div className = "contact">
+                <div class = "contact-table">
+                    <input type="text" class="input-form" name="name" placeholder="Name" required /><br/>
+                    <input type="text" class="input-form" name="email" placeholder="Email" required /><br/>
+                    <input type="text" class="input-form" name="subject" placeholder="Subject" required /><br/>
+                    <textarea class="input-form message-form" name="message" placeholder="Message" required /><br/>
+                </div>
+                <button class="submit-btn" >
+                    <span>Submit </span>
+                </button>
             </div>
         </section>
     )
