45f206e7c82dd59cc79c5592f7b0ab028a06b34d
wisemuji <wisemuji@gmail.com>
2019-05-26 23:40:54 +0900

    development journal week #4

--- app/http/app/src/component/development/week4.js
+++ app/http/app/src/component/development/week4.js
@@ -1,4 +1,53 @@
-const week4 = `## 개발일지 Week #4 (Not created yet)
-* * *`;
+const week4 = `## 개발일지 Week #4 (Created at 2019.05.26)
+* * *
+
+### Development period
+
+[개발 일정](https://www.notion.so/03d744e0c01c44a99527b47624956ad0)
+
+### Development codes
+
+[https://github.com/wisemuji/media-english-project](https://github.com/wisemuji/media-english-project)
+
+### Commit messages
+
+[https://github.com/wisemuji/media-english-project/commits/master](https://github.com/wisemuji/media-english-project/commits/master)
+
+### New features
+
+- **Front-end 개발 진행**
+    - **Communicate Section 완성**
+
+        고민) 기획서에서는 홍보와 Contact의 목적으로 챗봇을 구현한다고 하였으나 랜딩 페이지에 적용하기에는 제품 소개의 목적이 흐려질 수 있다고 생각했음
+
+        결론) 고민 끝에 '프리젠트' 개발진들에게 직접 이메일을 보내 컨택할 수 있는 화면을 만들면 고객이나 투자자들과 소통하기 쉽지 않을까 생각함.
+
+        결론) 이름, 이메일, 제목, 내용을 입력하고 버튼을 클릭하면 바로 이메일이 보내지는 화면 구성을 채택함.
+
+        ![이미지](/communicate_image.png)
+
+    - **Website Favicon, Title 변경**
+
+        기존에 기본 파비콘(아이콘)과 제목으로 구성되어 있던 웹사이트를 변경함
+
+        ![이미지](/favicon_image.png)
+
+    - **Button Hover**
+
+        버튼에 마우스를 올리면 '>>' 기호가 생기며 크기가 변하는 애니메이션이 추가된다.
+
+        ![이미지](/btn_hover.gif)
+
+- **Back-end 개발 진행**
+
+    이메일을 보내기 위한 기능을 구현함.
+
+    **사용기술** : Flask 
+
+    **API** : Flask-Mail API 
+
+- **Week4 개발 일지 업로드**
+
+    쉽게 이미지나 코드를 Embed할 수 있는 [Markdown](https://ko.wikipedia.org/wiki/%EB%A7%88%ED%81%AC%EB%8B%A4%EC%9A%B4) 사용.`;
 
 export default week4 ;
