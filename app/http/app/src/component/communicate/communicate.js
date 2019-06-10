5c45945b6346b745acc4acd4c29a5bae47023ff8
wisemuji <wisemuji@gmail.com>
2019-06-11 02:03:39 +0900

    alert email sending state

--- app/http/app/src/component/communicate/communicate.js
+++ app/http/app/src/component/communicate/communicate.js
@@ -19,18 +19,28 @@ export class Communicate extends Component {
         [e.target.name]: e.target.value
       })
     }
-    handleClick = () => {
+    handleClick = (e) => {
         let form = new FormData()
         form.set('name', this.state.name)
         form.set('email',this.state.email)
         form.set('subject',this.state.subject)
         form.set('message',this.state.message)
         axios.post(apiURL+'/send-mail', form) 
-            .then( response => {
-                console.log('response : ', JSON.stringify(response, null, 2)) })
-            .catch( error => {
-                console.log('failed', error) 
+        .then( response => {
+            alert('이메일이 성공적으로 전송되었습니다.')
+            // 상태 초기화
+            this.setState({
+                name: '',
+                email: '',
+                subject: '',
+                message: ''
             })
+            console.log('response : ', JSON.stringify(response, null, 2)) })
+        .catch( error => {
+            alert('이메일 전송이 실패하였습니다.')
+            console.log('failed', error) 
+        })
+        alert('이메일 전송중입니다.')
     }
         
     render() {
@@ -47,9 +57,7 @@ export class Communicate extends Component {
                         <input value={this.state.subject} onChange={this.handleChange} type="text" class="input-form" name="subject" placeholder="Subject" required /><br/>
                         <textarea value={this.state.message} onChange={this.handleChange} class="input-form message-form" name="message" placeholder="Message" required /><br/>
                     </div>
-                    <button class="submit-btn" onClick={ this.handleClick }>
-                        <span>Submit </span>
-                    </button>
+                    <input type="submit" class="submit-btn" onClick={ this.handleClick } value="Submit"/>
                 </div>
             </section>
         )  
